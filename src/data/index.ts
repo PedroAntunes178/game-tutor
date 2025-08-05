6e32909e2770a5ffe0f9696f5f2ab8d4d611a236
Pedro Antunes <pedronmantunes@gmail.com>
2025-08-05 19:54:53 +0100

    [improvement] added more games!

--- src/data/index.ts
+++ src/data/index.ts
@@ -5,6 +5,42 @@ import twoTruthsOneLie from './games/two-truths-one-lie.json';
 import poker from './games/poker.json';
 import scavengerHunt from './games/scavenger-hunt.json';
 import cassinoMadeira from './games/cassino-madeira.json';
+import cardsAgainstHumanity from './games/cards-against-humanity.json';
+import explodingKittens from './games/exploding-kittens.json';
+import spades from './games/spades.json';
+import rummy from './games/rummy.json';
+import goFish from './games/go-fish.json';
+import euchre from './games/euchre.json';
+import phase10 from './games/phase-10.json';
+import bridge from './games/bridge.json';
+import wouldYouRather from './games/would-you-rather.json';
+import humanBingo from './games/human-bingo.json';
+import neverHaveIEver from './games/never-have-i-ever.json';
+import theNameGame from './games/the-name-game.json';
+import speedFriending from './games/speed-friending.json';
+import thisOrThat from './games/this-or-that.json';
+import whoAmI from './games/who-am-i.json';
+import commonGround from './games/common-ground.json';
+import mMicebreaker from './games/m&m-icebreaker.json';
+import catan from './games/catan.json';
+import ticketToRide from './games/ticket-to-ride.json';
+import scrabble from './games/scrabble.json';
+import clue from './games/clue.json';
+import risk from './games/risk.json';
+import pandemic from './games/pandemic.json';
+import carcassonne from './games/carcassonne.json';
+import chess from './games/chess.json';
+import codenames from './games/codenames.json';
+import pictionary from './games/pictionary.json';
+import taboo from './games/taboo.json';
+import headsUp from './games/heads-up.json';
+import mafiaWerewolf from './games/mafia-werewolf.json';
+import truthOrDare from './games/truth-or-dare.json';
+import jackboxGames from './games/jackbox-games.json';
+import minuteToWinIt from './games/minute-to-win-it.json';
+import beerPong from './games/beer-pong.json';
+import telephone from './games/telephone.json';
+import sueca from './games/sueca.json';
 
 const games = [
   uno,
@@ -13,7 +49,43 @@ const games = [
   twoTruthsOneLie,
   poker,
   scavengerHunt,
-  cassinoMadeira
+  cassinoMadeira,
+  cardsAgainstHumanity,
+  explodingKittens,
+  spades,
+  rummy,
+  goFish,
+  euchre,
+  phase10,
+  bridge,
+  wouldYouRather,
+  humanBingo,
+  neverHaveIEver,
+  theNameGame,
+  speedFriending,
+  thisOrThat,
+  whoAmI,
+  commonGround,
+  mMicebreaker,
+  catan,
+  ticketToRide,
+  scrabble,
+  clue,
+  risk,
+  pandemic,
+  carcassonne,
+  chess,
+  codenames,
+  pictionary,
+  taboo,
+  headsUp,
+  mafiaWerewolf,
+  truthOrDare,
+  jackboxGames,
+  minuteToWinIt,
+  beerPong,
+  telephone,
+  sueca
 ];
 
 export default games;
